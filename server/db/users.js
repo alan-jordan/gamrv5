a37763bcfcea876f24c7be3d21c8e668e031a082
Alan Jordan <alanpjordan@gmail.com>
2018-04-16 23:54:39 +1200

    Passport working with react. Need to tidy up authentication so that it handles errors gracefully

--- server/db/users.js
+++ server/db/users.js
@@ -1,31 +1,31 @@
-var hash = require('../auth/hash')
+var hash = require("../auth/hash");
 
-function createUser (user_name, password, db) {
-  return new Promise ((resolve, reject) => {
+function createUser(user_name, password, db) {
+  return new Promise((resolve, reject) => {
     hash.generate(password, (err, hash) => {
-      if (err) reject(err)
-      db('users')
-        .insert({user_name, hash})
-        .then(user_id => resolve(user_id))
-    })
-
-  })
+      if (err) reject(err);
+      db("users")
+        .insert({ user_name, hash })
+        .then(user_id => resolve(user_id));
+    });
+  });
 }
-function userExists (user_name, db) {
-  console.log({user_name});
-  return db('users')
-    .where('user_name', user_name)
-    .then(user => !!user)
+function userExists(user_name, db) {
+  return db("users")
+    .where("user_name", user_name)
+    .then(user => {
+      return user.length === 0 ? false : true;
+    });
 }
 
-function getUserByName (user_name, db) {
-  return db('users')
-    .where('user_name', user_name)
-    .first()
+function getUserByName(user_name, db) {
+  return db("users")
+    .where("user_name", user_name)
+    .first();
 }
 
 module.exports = {
   createUser,
   userExists,
   getUserByName
-}
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,20 @@
-var router = require('express').Router()
+var router = require("express").Router();
 
-var {userExists, createUser} = require('../db/users')
-var token = require('../auth/token')
+var { userExists, createUser } = require("../db/users");
+var token = require("../auth/token");
 
-router.post('/register', register, token.issue)
+router.post("/register", register, token.issue);
 
-function register (req, res, next) {
-  const {user_name, password} = req.body
-  userExists(user_name, req.app.get('db'))
+function register(req, res, next) {
+  const { user_name, password } = req.body;
+  userExists(user_name, req.app.get("db"))
     .then(exists => {
-      if (exists) return res.status(400).send({message: "User exists"})
-      createUser(user_name, password, req.app.get('db'))
-        .then(() => next())
+      if (exists) return res.status(400).send({ message: "User exists" });
+      createUser(user_name, password, req.app.get("db")).then(() => next());
     })
-    .catch(err => res.status(500).send({message: err.message}))
+    .catch(err => res.status(500).send({ message: err.message }));
 }
 
+router.post("/login", token.issue);
 
-
-router.post('/login', token.issue)
-
-module.exports = router
+module.exports = router;
